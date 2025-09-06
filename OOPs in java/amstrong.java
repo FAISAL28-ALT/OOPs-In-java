3d26b54e5ac26b23b3b8e4f2154bdab1baeef8f2
FAISAL28-ALT <faisalkhancs79@gmail.com>
2025-09-06 11:57:50 +0530

    Update amstrong.java

--- OOPs in java/amstrong.java
+++ OOPs in java/amstrong.java
@@ -1,1 +1,28 @@
 
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Input the number to check Amstrong number: ");
+        int n=sc.nextInt();
+        int amg=0;
+        int temp=n;
+        int c=0;
+        while(temp!=0){
+            temp=temp/10;
+            c++;
+        }
+        temp=n;
+        while(temp!=0){
+            int d=temp%10;
+            amg+=(int)Math.pow(d,c);
+            temp=temp/10;
+        }
+        if(n==amg){
+            System.out.print("Number is amstrong: "+n);
+        }
+        else{
+            System.out.print("Number is not amstrong: "+n);
+        }
+    }
+}
